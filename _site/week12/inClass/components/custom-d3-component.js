cd968d2b077e9a9d9ec4dd4559ad6cde8d574f62
Jill Naiman <jnaiman@gmail.com>
2021-11-08 10:30:28 -0600

    Auto-commit: Mon Nov  8 10:30:28 CST 2021

--- _site/week12/inClass/components/custom-d3-component.js
+++ _site/week12/inClass/components/custom-d3-component.js
@@ -8,20 +8,21 @@ class CustomD3Component extends D3Component {
   // initialization function
   initialize(node, props) {
     const svg = (this.svg = d3.select(node).append('svg'));
-    svg // probably the background object
+    svg // probably the background object -- background canvas
       .attr('viewBox', `0 0 ${size} ${size}`)
       .style('width', '100%')
       .style('height', 'auto');
 
-    svg // drawing initial circle
+    svg // drawing initial circle -- changed to center and larger
 	  .append('circle')
 	  .attr('r', 200)
 	  .attr('cx', 300)
 	  .attr('cy',300);
       //.attr('cx', Math.random() * size)
       //.attr('cy', Math.random() * size);
-  }
+  } // end initialization function
 
+  //updating based on our interaction (button push)
   update(props, oldProps) {
     this.svg
       .selectAll('circle')
