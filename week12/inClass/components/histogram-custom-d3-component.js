4880f18b324752a4d34b5255a014f3b5c454d9b7
Jill Naiman <jnaiman@gmail.com>
2021-11-08 10:45:23 -0600

    Auto-commit: Mon Nov  8 10:45:23 CST 2021

--- week12/inClass/components/histogram-custom-d3-component.js
+++ week12/inClass/components/histogram-custom-d3-component.js
@@ -8,21 +8,41 @@ var margin = {top: 20, right: 20, bottom: 70, left: 40},
     height = 400 - margin.top - margin.bottom;
 
 class HistogramCustomD3Component extends D3Component {
-  // initialization function
-  initialize(node, props) {
-    const svg = (this.svg = d3.select(node).append('svg'));
-    svg // probably the background object -- background canvas
-      .attr('viewBox', `0 0 ${size} ${size}`)
-      .style('width', '100%')
-      .style('height', 'auto');
+    // initialization function
+    initialize(node, props) {
+	// Parse the date / time
+	var parseDate = d3.isoParse
+	// setting x-scale
+	var x = d3.scaleBand().rangeRound([0, width], .05).padding(0.1);
+	// setting y-scale
+	var y = d3.scaleLinear().range([height, 0]);
+	// x axis
+	var xAxis = d3.axisBottom()
+	    .scale(x)
+	    //.tickFormat(d3.timeFormat("%b"));
+	    .tickFormat(d3.timeFormat("%Y")); // reformat for years in our data
+	// y axis
+	var yAxis = d3.axisLeft()
+	    .scale(y)
+	    .ticks(10);
 
-    svg // drawing initial circle -- changed to center and larger
-	  .append('circle')
-	  .attr('r', 200)
-	  .attr('cx', 300)
-	  .attr('cy',300);
-      //.attr('cx', Math.random() * size)
-      //.attr('cy', Math.random() * size);
+	// start drawing things
+	const svg = (this.svg = d3.select(node).append('svg'));
+	svg // probably the background object -- background canvas
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	    .append("g")
+	    .attr("transform",
+		  "translate(" + margin.left + "," + margin.top + ")");
+	    //.attr('viewBox', `0 0 ${size} ${size}`)
+	    //.style('width', '100%')
+	    //.style('height', 'auto');
+
+	svg // drawing initial circle -- changed to center and larger
+	    .append('circle')
+	    .attr('r', 200)
+	    .attr('cx', 300)
+	    .attr('cy',300);
   } // end initialization function
 
   //updating based on our interaction (button push)
