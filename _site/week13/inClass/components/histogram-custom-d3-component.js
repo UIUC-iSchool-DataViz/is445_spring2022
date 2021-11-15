91206520eb621b68a56e171c2be06218cec1b627
Jill Naiman <jnaiman@gmail.com>
2021-11-15 10:14:11 -0600

    Auto-commit: Mon Nov 15 10:14:11 CST 2021

--- _site/week13/inClass/components/histogram-custom-d3-component.js
+++ _site/week13/inClass/components/histogram-custom-d3-component.js
@@ -65,8 +65,9 @@ class HistogramCustomD3Component extends D3Component {
 		.call(xAxis.ticks(null).tickSize(0))
 		.selectAll("text")
 		.style("text-anchor", "middle")
-		.attr("transform","rotate(-65)"); // rotate ticks by -65 deg
-		.style("text-anchor","end");
+		.attr("transform","rotate(-65)") // rotate ticks by -65 deg
+		.style("text-anchor","end") // changing anchor to end
+		.style("font-size","6px"); // smaller font size
 
 	    // drawing y-axis
 	    svg.append("g")
