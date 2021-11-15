bd743d81aa363b787ec10993951d340b81ad4d66
Jill Naiman <jnaiman@gmail.com>
2021-11-15 10:15:14 -0600

    Auto-commit: Mon Nov 15 10:15:14 CST 2021

--- _site/week13/inClass/components/histogram-custom-d3-component.js
+++ _site/week13/inClass/components/histogram-custom-d3-component.js
@@ -4,7 +4,7 @@ const d3 = require('d3');
 
 //const size = 600;
 var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = 1200 - margin.left - margin.right,
+    width = 800 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
 class HistogramCustomD3Component extends D3Component {
