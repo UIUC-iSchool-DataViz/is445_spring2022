1bff160c2028a8248006e9e9e5b476273106c89e
jnaiman <jnaiman@illinois.edu>
2022-04-11 16:55:54 -0500

    Auto-commit: Mon Apr 11 16:55:54 CDT 2022

--- week13/inClass/components/histogram-custom-d3-component.js
+++ week13/inClass/components/histogram-custom-d3-component.js
@@ -50,6 +50,60 @@ class HistogramCustomD3Component extends D3Component {
 	    .attr("transform",
 		  "translate(" + margin.left + "," + margin.top + ")");
 
+
+	d3.csv("bar-data.csv", function(error, data) {
+
+	    data.forEach(function(d) {
+		d.date = parseDate(d.date);
+		d.value = +d.value;
+	    });
+	    
+	    x.domain(data.map(function(d) { return d.date; }));
+	    y.domain([0, d3.max(data, function(d) { return d.value; })]);
+	    
+	    svg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis.ticks(null).tickSize(0))
+		.selectAll("text")
+		.style("text-anchor", "middle")
+	    //       .attr("dx", "-.8em")
+	    //       .attr("dy", "-.55em")
+	    //       .attr("transform", "rotate(-90)" );
+	    
+	    svg.append("g")
+		.attr("class", "y axis")
+		.call(yAxis.ticks(null).tickSize(0))
+		.append("text")
+	    //       .attr("transform", "rotate(-90)")
+		.attr("y", 6)
+	    //       .attr("dy", ".71em")
+		.style("text-anchor", "middle")
+		.text("Value");
+	    
+	    svg.selectAll("bar")
+		.data(data)
+		.enter().append("rect")
+		.style("fill", function(d){ return d.value < d.target ? '#EF5F67': '#3FC974'})
+		.attr("x", function(d) { return x(d.date); })
+		.attr("width", x.bandwidth())
+		.attr("y", function(d) { return y(d.value); })
+		.attr("height", function(d) { return height - y(d.value); });
+	    svg.selectAll("lines")
+		.data(data)
+		.enter().append("line")
+		.style("fill", 'none')
+  		.attr("x1", function(d) { return x(d.date) + x.bandwidth()+5; })
+		.attr("x2", function(d) { return x(d.date)-5; })
+		.attr("y1", function(d) { return y(d.target); })
+		.attr("y2", function(d) { return y(d.target); })
+  		.style("stroke-dasharray", [2,2])
+  		.style("stroke", "#000000")
+		.style("stroke-width", 2)
+	    
+	}); // end of d3.csv function
+
+	
     //svg
     //  .append('circle')
     //  .attr('r', 20) // radius of the circle
