8b17051f66f877eb30c7ef24a414eef8b515279b
jnaiman <jnaiman@illinois.edu>
2022-04-11 17:20:16 -0500

    Auto-commit: Mon Apr 11 17:20:16 CDT 2022

--- week13/inClass/components/histogram-custom-d3-component.js
+++ week13/inClass/components/histogram-custom-d3-component.js
@@ -81,6 +81,7 @@ class HistogramCustomD3Component extends D3Component {
 		.call(xAxis.ticks(null).tickSize(0))
 		.selectAll("text")
 		.style("text-anchor", "middle")
+		.attr("transform", "rotate(-90)" );
 	    
 	    svg.append("g")
 		.attr("class", "y axis")
